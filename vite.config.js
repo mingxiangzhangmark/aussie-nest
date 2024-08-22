32ba6589a3ac50d21528dce34892b125f4390799
mingxiangzhang_mark@outlook.com <mingxiangzhang_mark@outlook.com>
2024-08-22 20:24:31 +1000

    change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // 使用相对路径
   server:{
     port:3000
   },
@@ -11,5 +12,5 @@ export default defineConfig({
   resolve: {
     mainFields: [],
   },
-  base: './',  // 使用相对路径
+  
 })
